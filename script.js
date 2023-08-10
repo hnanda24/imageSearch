99210d50778afc7280054962fab7aec23bcff429
Hardik Nanda <108220239+hnanda24@users.noreply.github.com>
2023-08-10 19:11:40 +0530

    Add files via upload
    First Commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const accesskey = "DqHP1nWobo7f3tq1Q7fqrDYkaI_-PLVJeBb58RE0zhc";
+
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("search-more-btn");
+
+let keyword = "";
+let page = 1;
+
+async function searchImage()
+{
+    keyword = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accesskey}&per_page=12`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if(page === 1)
+    {
+        searchResult.innerHTML = "";
+    }
+    
+    // console.log(data);
+    const results = data.results;
+
+    results.map((result)=>{
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+
+        imageLink.appendChild(image);
+        searchResult.appendChild(imageLink);
+    })
+    document.getElementById('show-more-btn').style.visibility = "visible";
+}
+
+document.getElementById('search-form').addEventListener("submit",(e) =>{
+    e.preventDefault();
+    page = 1;
+    searchImage();
+})
+
+document.getElementById('show-more-btn').addEventListener("click",()=>{
+    page++;
+    searchImage();
+})
